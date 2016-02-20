d7b343c69045bd8bfbef92a0eecf26dd7d920731
jojoldu <jojoldu@gmail.com>
2016-02-20 17:49:50 +0900

    boj 간단한 문자열 문제 풀이

---
+++ src/backjun/string/alphabet/Main.java
@@ -0,0 +1,31 @@
+package backjun.string.alphabet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-02-20.
+ */
+/*
+    알파벳 소문자로만 이루어진 단어 S가 주어진다.
+    이 때, 각 알파벳이 단어에 몇 개가 포함되어 있는지 구하는 프로그램을 작성하시오.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int length = input.length();
+        int[] result = new int[26];
+        Arrays.fill(result, 0);
+
+        for(int i=0;i<length;i++){
+            result[input.charAt(i)-97]++;
+        }
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+    }
+}
---
+++ src/backjun/string/dial/Main.java
@@ -0,0 +1,31 @@
+package backjun.string.dial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-02-20.
+ */
+/*
+    https://www.acmicpc.net/problem/5622
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int length = input.length();
+        int[] scope = {64, 67, 70, 73, 76, 79, 83, 86, 90};
+        int result=0;
+        int c;
+        for(int i=0;i<length;i++){
+            c = input.charAt(i);
+            for(int j=0;j<scope.length-1;j++){
+                if(c > scope[j] && c<=scope[j+1]){
+                    result+=j+3;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
