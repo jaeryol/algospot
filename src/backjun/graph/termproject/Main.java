1c830bb99ca87abb138068be5f2ced03a57a31b6
jojoldu <jojoldu@gmail.com>
2016-03-01 18:05:33 +0900

    [#9466] 타임아웃 자꾸 뜨네 미치게

--- src/backjun/graph/termproject/Main.java
+++ src/backjun/graph/termproject/Main.java
@@ -1,5 +1,7 @@
 package backjun.graph.termproject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,44 +11,48 @@ import java.util.Scanner;
     https://www.acmicpc.net/problem/9466
  */
 public class Main {
-    private static int count = 0;
+
+    public static int[] numbers = new int[100001];
+    public static int[] positions = new int[100001];
+    public static int[] visited = new int[100001];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int inputCount = sc.nextInt();
-        int[] array;
-        boolean[] visited;
         for(int i=0;i<inputCount;i++){
             int size = sc.nextInt();
-            visited = new boolean[size+1];
-            array = new int[size+1];
 
             for(int j=1;j<=size;j++){
-                array[j] = sc.nextInt();
+                numbers[j] = sc.nextInt();
+                positions[j] = 0;
+                visited[j] = 0;
             }
 
-            for(int j=1;j<=size;j++){
-
-                dfs(array, visited, j, j);
+            int result=0;
+            for(int j=0;j<=size;j++){
+                if(positions[i] == 0){
+                    result += dfs(i, 1, i);
+                }
             }
-
-            System.out.println(count);
+            System.out.println(size - result);
         }
     }
 
-    public static void dfs(int[] array, boolean[] visited, int start, int current){
-
-        if(start == array[current]){
-            return;
+    /*
+       1. 시작점이 곧 끝점이어야 한다.
+       2. 시작점을 다시 만나기전에 이미 방문한 점을 만나면 안된다.
+       3. 이미 완전 한조가 되었다면 제외
+     */
+    public static int dfs(int current, int count, int step){
+        if(positions[current] != 0){
+            if(visited[current] != step){
+                return 0;
+            }
+            return count - positions[current];
         }
-
-        if(visited[current]){
-            count++;
-            return;
-        }
-
-        visited[current] = true;
-        dfs(array, visited, start, array[current]);
+        positions[current] = count;
+        visited[current] = step;
+        return dfs(numbers[current], count+1, step);
     }
 }
